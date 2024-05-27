a38e6d70f02ddb3d0f08237728bc61332560fdd6
Oscar González Baldizon <oscar.gonzalez.baldizon@gmail.com>
2024-05-27 15:44:10 -0600

    a

---
+++ multiplesLenguajes/C/hola.c
@@ -0,0 +1,15 @@
+/* Este codigo ha sido generado por el modulo psexport 20180802-w32 de PSeInt.
+Es posible que el codigo generado no sea completamente correcto. Si encuentra
+errores por favor reportelos en el foro (http://pseint.sourceforge.net). */
+
+#include<stdio.h>
+
+int main() {
+	int n1;
+	printf("Hola Mundo\n");
+	printf("Ingresa el dato:\n");
+	scanf("%i",&n1);
+	printf("%i\n",n1);
+	return 0;
+}
+
---
+++ multiplesLenguajes/C/hola.js
@@ -0,0 +1,12 @@
+// Este codigo ha sido generado por el modulo psexport 20180802-w32 de PSeInt.
+// Es posible que el codigo generado no sea completamente correcto. Si encuentra
+// errores por favor reportelos en el foro (http://pseint.sourceforge.net).
+
+function sin_titulo() {
+	var n1 = new Number();
+	document.write("Hola Mundo",'<BR/>');
+	document.write("Ingresa el dato:",'<BR/>');
+	n1 = Number(prompt());
+	document.write(n1,'<BR/>');
+}
+
