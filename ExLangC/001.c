f91bfd3254c99be844c3df2e60bf2a1487e42696
Oscar González Baldizon <oscar.gonzalez.baldizon@gmail.com>
2024-05-27 14:45:29 -0600

    C

---
+++ ExLangC/001.c
@@ -0,0 +1,14 @@
+/* Este codigo ha sido generado por el modulo psexport 20180802-w32 de PSeInt.
+Es posible que el codigo generado no sea completamente correcto. Si encuentra
+errores por favor reportelos en el foro (http://pseint.sourceforge.net). */
+
+#include<stdio.h>
+
+int main() {
+	int n1;
+	printf("Ingresa el dato:\n");
+	scanf("%i",&n1);
+	printf("%i es un numero entero\n",n1);
+	return 0;
+}
+
